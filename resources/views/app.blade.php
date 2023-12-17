ae78be4430ae2d415241b9f53d1bca77243bd7b6
abhi1221snva <42020778+abhi1221snva@users.noreply.github.com>
2023-12-17 13:27:52 +0530

    cafmotel_laravel_new_site

---
+++ resources/views/app.blade.php
@@ -0,0 +1,244 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Links of CSS files -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <link rel="stylesheet" href="assets/css/fancybox.min.css">
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/dark.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <title>Cafmotel-Be Inspired By Communication</title>
+
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <style>
+        .subline{
+            font-size: 24px;
+    line-height: 31px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- Start Navbar Area -->
+    
+<!-- Start Navbar Area -->
+    <div class="navbar-area navbar-color-white">
+        <div class="buzz-responsive-nav">
+            <div class="container">
+                <div class="buzz-responsive-menu">
+                    <div class="logo">
+                        <a href="/">
+                            <img src="assets/img/logo-white.png" alt="logo">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="buzz-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <a class="navbar-brand" href="/">
+                        <img src="assets/img/logo-white.png" alt="logo">
+                    </a>
+
+                    <div class="collapse navbar-collapse mean-menu">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+                           
+
+                            <li class="nav-item"><a href="#" class="nav-link">Services <i
+                                        class='bx bx-chevron-down'></i></a>
+                                <ul class="dropdown-menu">
+                                     <li class="nav-item"><a href="" class="nav-link">Virtual Number <i class="bx bx-chevron-down"></i></a>
+                                        <ul  class="dropdown-menu">
+                                            <li class="nav-item"><a href="usa-virtual-phone-number.php" class="nav-link">USA</a></li>
+                                            <li class="nav-item"><a href="canada-virtual-phone-number.php" class="nav-link">Canada</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item"><a href="smart-ivr-system.php" class="nav-link">Smart IVR system</a>
+
+                                    </li>
+                                    <li class="nav-item"><a href="call-center-services.php" class="nav-link">Distributed Call Center</a>
+                                    </li>
+                                    <!-- <li class="nav-item"><a href="phone-number-masking.php" class="nav-link">Number masking</a></li> -->
+                                    <li class="nav-item"><a href="" class="nav-link">Toll Free Number <i class="bx bx-chevron-down"></i></a>
+                                        <ul  class="dropdown-menu">
+                                            <li class="nav-item"><a href="usa-toll-free-number.php" class="nav-link">USA</a></li>
+                                            <li class="nav-item"><a href="canada-toll-free-number.php" class="nav-link">Canada</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item"><a href="outbound-call-service.php" class="nav-link">Outbound Call Service</a></li>
+                                    <li class="nav-item"><a href="call-conferencing-service.php" class="nav-link">Conferencing</a></li>
+                                    <li class="nav-item"><a href="e-fax.php" class="nav-link">E-Fax</a></li>
+                                    <li class="nav-item"><a href="messaging.php" class="nav-link">Messaging</a></li>
+                                    <li class="nav-item"><a href="click-to-call.php" class="nav-link">Click to Call</a></li>
+                                    <!-- <li class="nav-item"><a href="missed-call-service.php" class="nav-link">Missed Call Services</a></li> -->
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a href="conversationalai.php" class="nav-link">Conversational AI</a></li>
+                            <li class="nav-item"><a href="reseller-program.php" class="nav-link">Reseller Program </a></li>
+                            
+
+                            <li class="nav-item"><a href="career.php" class="nav-link">Career</a></li>
+                            <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+                        </ul>
+
+                        
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        
+    </div>
+    <!-- End Navbar Area -->    <!-- End Navbar Area -->
+
+    
+
+            @yield('content')
+   
+    <!-- Start Footer Area -->
+    <!-- Start Footer Area -->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <a href="/" class="logo">
+                            <img src="assets/img/logo-white.png" alt="logo">
+                        </a>
+                        <p>We at Cafmotel help every small or big business to rule the world with the smartest communication solutions. Making it simple and straightforward to generate leads, building relations, and converting the interaction into a deal, we help the enterprises to grow their revenue through our all-in-one "Voice, Text, Email, and Fax" platform by holding hands through the entire process.</p>
+
+                        <ul class="social-link">
+                            <li><a href="https://www.facebook.com/Cafmotel" class="d-block" target="_blank"><i
+                                        class='bx bxl-facebook'></i></a></li>
+                            <li><a href="https://twitter.com/Cafmotel" class="d-block" target="_blank"><i
+                                        class='bx bxl-twitter'></i></a></li>
+                            <li><a href="https://www.instagram.com/Cafmotel/" class="d-block" target="_blank"><i
+                                        class='bx bxl-instagram'></i></a></li>
+                            <li><a href="https://www.linkedin.com/company/Cafmotel" class="d-block"
+                                    target="_blank"><i class='bx bxl-linkedin'></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget pl-5">
+                        <h3>Explore</h3>
+
+                        <ul class="footer-links-list">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="conversationalai.php">Conversational AI</a></li>
+                            <li><a href="reseller-program.php">Reseller Program</a></li>
+                            
+                            <li><a href="career.php">Careers</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+
+                            
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget pl-2">
+                        <h3>Featured Services</h3>
+
+                        <ul class="footer-links-list">
+                        <li><a href="canada-virtual-phone-number.php">Virtual phone number</a></li>
+                            <li><a href="smart-ivr-system.php">Best Smart IVR Solution</a></li>
+                            <li><a href="canada-toll-free-number.php">Buy Toll Free Number</a></li>
+                            <li><a href="call-center-services.php">Best Call Center Services</a></li>
+                            <li><a href="outbound-call-service.php">Outbound Call Service</a></li>
+                            <li><a href="phone-number-masking.php">Best Phone Number Masking</a></li>
+                            <li><a href="call-conferencing-service.php">Best Conference Call Service</a></li>
+                            <li><a href="missed-call-service.php">Best Missed Call Services</a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h3>Contact</h3>
+
+                        <ul class="footer-contact-info">
+                            <!-- <li><i class='bx bx-map'></i>175 5th Ave Premium Area, New York, NY 10010, United States
+                            </li> -->
+
+                            <li><i class='bx bx-phone-call'></i><a href="tel:
+                            +18446142007 ">
+                            +1 844-614-2007 </a></li>
+                            <li><i class='bx bx-envelope'></i><a
+                                    href="mailto:sales@cafmotel.com"><span>sales@cafmotel.com</span></a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-bottom-area">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-6">
+                        <p>© 2021 All rights reserved <a href="#" target="_blank"> Cafmotel</a>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <!-- <ul>
+                            <li><a href="privacy-policy.php">Privacy Policy</a></li>
+                            <li><a href="terms-of-service.php">Terms & Conditions</a></li>
+                        </ul> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="shape16"><img src="assets/img/shape/shape16.png" alt="image"></div>
+    </footer>
+    <!-- End Footer Area -->
+
+    <div class="go-top"><i class='bx bx-up-arrow-alt'></i></div>
+
+    <!-- Links of JS files -->
+    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/appear.min.js"></script>
+    <script src="assets/js/odometer.min.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/fancybox.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/meanmenu.min.js"></script>
+    <script src="assets/js/nice-select.min.js"></script>
+    <script src="assets/js/sticky-sidebar.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.js"></script>
+    <script src="assets/js/ajaxchimp.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    
+    <!-- End Footer Area -->
+
+</body>
+
+
+</html>
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,128 @@
+
+    <!-- Start Footer Area -->
+    <!-- Start Footer Area -->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <a href="/" class="logo">
+                            <img src="assets/img/logo-white.png" alt="logo">
+                        </a>
+                        <p>We at Cafmotel help every small or big business to rule the world with the smartest communication solutions. Making it simple and straightforward to generate leads, building relations, and converting the interaction into a deal, we help the enterprises to grow their revenue through our all-in-one "Voice, Text, Email, and Fax" platform by holding hands through the entire process.</p>
+
+                        <ul class="social-link">
+                            <li><a href="https://www.facebook.com/Cafmotel" class="d-block" target="_blank"><i
+                                        class='bx bxl-facebook'></i></a></li>
+                            <li><a href="https://twitter.com/Cafmotel" class="d-block" target="_blank"><i
+                                        class='bx bxl-twitter'></i></a></li>
+                            <li><a href="https://www.instagram.com/Cafmotel/" class="d-block" target="_blank"><i
+                                        class='bx bxl-instagram'></i></a></li>
+                            <li><a href="https://www.linkedin.com/company/Cafmotel" class="d-block"
+                                    target="_blank"><i class='bx bxl-linkedin'></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget pl-5">
+                        <h3>Explore</h3>
+
+                        <ul class="footer-links-list">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="conversationalai.php">Conversational AI</a></li>
+                            <li><a href="reseller-program.php">Reseller Program</a></li>
+                            
+                            <li><a href="career.php">Careers</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+
+                            
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget pl-2">
+                        <h3>Featured Services</h3>
+
+                        <ul class="footer-links-list">
+                        <li><a href="canada-virtual-phone-number.php">Virtual phone number</a></li>
+                            <li><a href="smart-ivr-system.php">Best Smart IVR Solution</a></li>
+                            <li><a href="canada-toll-free-number.php">Buy Toll Free Number</a></li>
+                            <li><a href="call-center-services.php">Best Call Center Services</a></li>
+                            <li><a href="outbound-call-service.php">Outbound Call Service</a></li>
+                            <li><a href="phone-number-masking.php">Best Phone Number Masking</a></li>
+                            <li><a href="call-conferencing-service.php">Best Conference Call Service</a></li>
+                            <li><a href="missed-call-service.php">Best Missed Call Services</a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h3>Contact</h3>
+
+                        <ul class="footer-contact-info">
+                            <!-- <li><i class='bx bx-map'></i>175 5th Ave Premium Area, New York, NY 10010, United States
+                            </li> -->
+
+                            <li><i class='bx bx-phone-call'></i><a href="tel:
+                            +18446142007 ">
+                            +1 844-614-2007 </a></li>
+                            <li><i class='bx bx-envelope'></i><a
+                                    href="mailto:sales@cafmotel.com"><span>sales@cafmotel.com</span></a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-bottom-area">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-6">
+                        <p>© 2021 All rights reserved <a href="#" target="_blank"> Cafmotel</a>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <!-- <ul>
+                            <li><a href="privacy-policy.php">Privacy Policy</a></li>
+                            <li><a href="terms-of-service.php">Terms & Conditions</a></li>
+                        </ul> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="shape16"><img src="assets/img/shape/shape16.png" alt="image"></div>
+    </footer>
+    <!-- End Footer Area -->
+
+    <div class="go-top"><i class='bx bx-up-arrow-alt'></i></div>
+
+    <!-- Links of JS files -->
+    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/appear.min.js"></script>
+    <script src="assets/js/odometer.min.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/fancybox.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/meanmenu.min.js"></script>
+    <script src="assets/js/nice-select.min.js"></script>
+    <script src="assets/js/sticky-sidebar.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.js"></script>
+    <script src="assets/js/ajaxchimp.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    
+    <!-- End Footer Area -->
+
+</body>
+
+
+</html>
